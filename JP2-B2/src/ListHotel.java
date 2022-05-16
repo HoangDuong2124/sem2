300d59b7f3c421a74b4a52f180dbf5b1f53e980b
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2022-05-16 03:01:08 -0700

    BTArrayList
    array

--- JP2-B2/src/ListHotel.java
+++ JP2-B2/src/ListHotel.java
@@ -4,12 +4,18 @@ import java.util.List;
 public class ListHotel {
     public static void main(String[] args) {
         List<Hotel> listHotel =new ArrayList<>();
-        listHotel.add(new Hotel("Nha Trang Hotel","Nha Trang","Resort hotel",5));
+        listHotel.add(new Hotel("Nha Trang Hotel","Khanh Hoa","Resort hotel",5));
         listHotel.add(new Hotel("Da Lat Hotel","Lam Dong","Commercial hotel",4));
         listHotel.add(new Hotel("KonTum Hotel","Kon Tum","Hostel",3));
+
 
         listHotel.forEach(listHotels ->{
             System.out.println(listHotels);
         });
+        String resort = String.valueOf(listHotel.get(0));
+        System.out.println("Resort Hotel in Nha Trang la: "+resort);
+
+        listHotel.set(0,new Hotel("Nha Trang Hotel","Khanh Hoa","Floating hotel",5));
+        System.out.println("Update: "+listHotel.get(0));
     }
 }
